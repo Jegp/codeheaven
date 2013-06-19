2d85be47b52be7cc4a3aaf1eb989bb8568b4545a
jegp <jensep@gmail.com>
2013-06-19 13:09:11 +0200

    Added basic http server

--- src/scala/codeheaven/HttpServer.scala
+++ src/scala/codeheaven/HttpServer.scala
@@ -1,12 +1,32 @@
 package scala.codeheaven
 
+import java.net.{Socket, ServerSocket}
+import java.io.{OutputStreamWriter, InputStreamReader, BufferedReader, PrintWriter}
+import java.util.Scanner
+
 /**
  * The HTTP entry-point for codeheaven.
  */
 object HttpServer {
 
   def main(args : Array[String]) {
-    println("überness")
+    val server = new ServerSocket(8080)
+    while (true) {
+      val socket = server.accept()
+      val writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream))
+      writer.println("HTTP/1.1 200 OK")
+      writer.println("Host: localhost")
+      writer.println("Accept: */*")
+      writer.println("User-Agent: Java") // Be honest.
+      writer.println("Content-Length: 11")
+      writer.println() // Important, else the server will expect that there's more into the request.
+      writer.print("Hello world")
+      writer.flush()
+
+
+      writer.close()
+      socket.close()
+    }
   }
 
 }
